d4d23e323a7c9d1f747ec20f405fafc59e4a30d0
MiyaPeynova <maria.peynova@hotmail.com>
2021-08-20 08:28:24 +0100

    Add error message

--- src/script.js
+++ src/script.js
@@ -1,7 +1,14 @@
 const arrow = document.querySelector("arrow");
 const speed = document.querySelector(".speed");
 
-navigator.geolocation.watchPosition((data) => {
-  console.log(data);
-  speed.textContent = data.coords.speed;
-});
+navigator.geolocation.watchPosition(
+  (data) => {
+    console.log(data);
+    speed.textContent = data.coords.speed;
+    arrow.getElementsByClassName.transform = `${data.coords.heading}deg`;
+  },
+  (err) => {
+    console.error(err);
+    alert("Wanna allow that to happen?!");
+  }
+);
